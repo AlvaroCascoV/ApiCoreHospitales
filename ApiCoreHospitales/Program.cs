90401e68e6fe9f50a3df653e52ee361b2524d0eb
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-03-24 14:19:08 +0100

    Add project files.

---
+++ ApiCoreHospitales/Program.cs
@@ -0,0 +1,31 @@
+using ApiCoreHospitales.Data;
+using ApiCoreHospitales.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddTransient<RepositoryHospital>();
+string connectionString = builder.Configuration.GetConnectionString("AZURETAJAMAR");
+builder.Services.AddDbContext<HospitalContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ApiCoreHospitales/Repositories/RepositoryHospital.cs
@@ -0,0 +1,25 @@
+﻿using ApiCoreHospitales.Data;
+using ApiCoreHospitales.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCoreHospitales.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+    
+        public async Task<List<Hospital>> GetHospitalesAsync()
+        {
+            return await this.context.Hospitales.ToListAsync();
+        }
+
+        public async Task<Hospital> FindHospitalAsync(int id)
+        {
+            return await this.context.Hospitales.FirstOrDefaultAsync(x => x.IdHospital == id);
+        }
+    }
+}
