e9e1b53f94bb61ae3ebad735f8995b731a219ab2
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-03-25 09:44:52 +0100

    hospitales scalar

--- ApiCoreHospitales/Program.cs
+++ ApiCoreHospitales/Program.cs
@@ -1,6 +1,7 @@
 using ApiCoreHospitales.Data;
 using ApiCoreHospitales.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,10 +18,12 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//}
+
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
